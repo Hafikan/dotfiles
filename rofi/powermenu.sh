141355aff604edd3a3be3dbd7749c52a949a4581
Hafikan <hafikanyesilyurt@gmail.com>
2026-04-02 17:50:14 +0300

    updated

---
+++ rofi/powermenu.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+## Rofi   : Power Menu
+
+
+# Current Theme
+dir="$HOME/.config/rofi/"
+theme='powermenu'
+
+# CMDs
+lastlogin="`last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7`"
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+# Options
+hibernate=''
+shutdown=''
+reboot=''
+lock=''
+suspend=''
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p " $USER@$host" \
+		-mesg " Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 240px;}' \
+		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--hibernate' ]]; then
+            systemctl hibernate
+        elif [[ $1 == '--suspend' ]]; then
+            pactl set-sink-mute @DEFAULT_SINK@ 1
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            if [[ -n "$SWAYSOCK" ]]; then
+                swaymsg exit
+            fi
+        fi
+    else
+        exit 0
+    fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+        run_cmd --shutdown
+        ;;
+    $reboot)
+        run_cmd --reboot
+        ;;
+    $hibernate)
+        run_cmd --hibernate
+        ;;
+    $lock)
+        swaylock
+        ;;
+    $suspend)
+        run_cmd --suspend
+        ;;
+    $logout)
+        run_cmd --logout
+        ;;
+esac
